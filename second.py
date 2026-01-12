a36ddb80e5fe4f3e9b66d63b02e8e4a7230f5ca7
rudra011-boss <rudrapratap83831@gmail.com>
2026-01-12 19:03:54 +0530

    qn1 complete

--- second.py
+++ second.py
@@ -24,3 +24,20 @@ print(name.replace("  "," "))# Strings are immutable which means that you cannot
 letter = "Dear harry,\n\t this python course is nice.\n Thanks!"
 print(letter)
 # ps 4
+# Qn 1
+fruit = []
+f1 = input("enter 1st fruit name :")
+fruit.append(f1)
+f2 = input("enter 2nd fruit name :")
+fruit.append(f2)
+f3 = input("enter 3rd fruit name :")
+fruit.append(f3)
+f4 = input("enter 4th fruit name :")
+fruit.append(f4)
+f5 = input("enter 5th fruit name :")
+fruit.append(f5)
+f6 = input("enter 6th fruit name :")
+fruit.append(f6)
+f7 = input("enter 7th fruit name :")
+fruit.append(f7)
+print(fruit)
