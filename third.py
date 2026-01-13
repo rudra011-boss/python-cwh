0d8a3379ee27c23ee60764fdf86e66ce896ab101
rudra011-boss <rudrapratap83831@gmail.com>
2026-01-13 18:24:35 +0530

    qn2 complete

--- third.py
+++ third.py
@@ -1,11 +1,28 @@
 #ps 5
 #qn 1
-hindi_dict= {"नमस्ते": "Hello",
+'''(hindi_dict= {"नमस्ते": "Hello",
     "पानी": "Water",
     "किताब": "Book",
     "घर": "House",
     "स्कूल": "School",
     "दोस्त": "Friend"
 }
-word = input("input hindi words to see their english translation: ")
-print("meaning =",hindi_dict.get(word))
+word = int(input("int(input hindi words to see their english translation: "))
+print("meaning =",hindi_dict.get(word)))'''
+#qn2
+s = set()
+n = int(input("enter number 1: "))
+s.add(n)
+n = int(input("enter number2: "))
+s.add(n)
+n = int(input("enter number 3: "))
+s.add(n)
+n = int(input("enter number 4: "))
+s.add(n)
+n = int(input("enter number 5: "))
+s.add(n)
+n = int(input("enter number 6: "))
+s.add(n)
+n = int(input("enter number 7: "))
+s.add(n)
+print(s)
